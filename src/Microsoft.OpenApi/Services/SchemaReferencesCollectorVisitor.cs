3dbff852cc15402c5d41c30b4fa173d019e800ff
Vincent Biret <vibiret@microsoft.com>
2026-01-14 13:02:16 -0500

    fix: walk schemas recursively to avoid broken references
    Signed-off-by: Vincent Biret <vibiret@microsoft.com>

--- src/Microsoft.OpenApi/Services/SchemaReferencesCollectorVisitor.cs
+++ src/Microsoft.OpenApi/Services/SchemaReferencesCollectorVisitor.cs
@@ -8,9 +8,40 @@ internal class SchemaReferencesCollectorVisitor : OpenApiVisitorBase
     public override void Visit(IOpenApiReferenceHolder referenceableHolder)
     {
         base.Visit(referenceableHolder);
-        if (referenceableHolder is OpenApiSchemaReference schemaReference && !string.IsNullOrEmpty(schemaReference.Reference?.Id) && schemaReference.Reference?.Id is not null)
+        if (referenceableHolder is OpenApiSchemaReference schemaReference &&
+            !string.IsNullOrEmpty(schemaReference.Reference?.Id) &&
+            schemaReference.Reference?.Id is not null &&
+            _visitedSchemasReferences.Add(schemaReference.Reference.Id) &&
+            schemaReference.RecursiveTarget is {} referencedSchema)
         {
-            _visitedSchemasReferences.Add(schemaReference.Reference.Id);
+            Visit((IOpenApiSchema)referencedSchema);
+        }
+    }
+    public override void Visit(IOpenApiSchema schema)
+    {
+        base.Visit(schema);
+        if (schema is IOpenApiReferenceHolder referenceHolder) Visit(referenceHolder);
+        if (schema is not OpenApiSchema openApiSchema) return;
+        VisitCollection(openApiSchema.AllOf);
+        VisitCollection(openApiSchema.AnyOf);
+        VisitCollection(openApiSchema.OneOf);
+        VisitCollection(openApiSchema.Properties?.Values);
+        if (openApiSchema.AdditionalProperties is not null)
+            Visit(openApiSchema.AdditionalProperties);
+        if (openApiSchema.Items is not null)
+            Visit(openApiSchema.Items);
+        if (openApiSchema.Not is not null)
+            Visit(openApiSchema.Not);
+        if (openApiSchema.Discriminator?.Mapping is not null)
+            VisitCollection(openApiSchema.Discriminator.Mapping.Values);
+        //TODO add default mapping in V3
+    }
+    private void VisitCollection(IEnumerable<IOpenApiSchema>? schemas)
+    {
+        if (schemas is null) return;
+        foreach (var schema in schemas)
+        {
+            Visit(schema);
         }
     }
     public HashSet<string> GetVisitedSchemasReferences()
